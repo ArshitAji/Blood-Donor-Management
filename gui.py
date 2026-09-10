b92dffa155121114d3ca4d53ff2cd924816c4f73
ArshitAji <arshitaji16@gmail.com>
2026-09-10 14:23:01 +0530

    Modified blood group as selection based

--- gui.py
+++ gui.py
@@ -10,10 +10,12 @@ tab1,tab2=st.tabs(["ADD","VIEW"])
 with tab1:
     st.title("Add New Blood Donor")
     name=st.text_input("Blood Donor Name=")
-    blood_group=st.text_input("Blood Group=")
+    # blood_group=st.text_input("Blood Group=")
+    #blood group using option
+    blood_group=st.selectbox("Please select your blood group",["A+","B+","O-","AB+","A-","B-","AB-","O+"])
     phone=st.text_input("Phone Number=")
     city=st.text_input("City Name=")
-    last_donation=st.text_input("Last donation date=")
+    last_donation=st.text_input("Last donation date=(yyyy/mm/dd)")
     if st.button("Add New Blood Donor"):
         donor_instance.post(name=name,blood_group=blood_group,phone=phone,city=city,last_donation=last_donation)
         st.success("Blood Donor Added Successfully")
